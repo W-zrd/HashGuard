d8fcb3b9a7cba24375779da5e81d9051311d7a4b
flxnzz <itszyro.47@gmail.com>
2023-06-28 06:28:45 +0700

    add recursive mode and test files

--- hashguard.py
+++ hashguard.py
@@ -1,13 +1,27 @@
 import hashlib
+import os
 
-def hash(path, crypto="md5"):
+def singleMode(path, crypto="md5"):
     obj = hashlib.new(crypto)
     with open(path, 'rb') as file:
         for i in iter(lambda: file.read(4096), b''):
             obj.update(i)
     return obj.hexdigest()
 
-# Usage example
-path = "/path/to/your/file.format"
-hashed = hash(path, crypto="sha256")
+def recursiveMode(directory_path, crypto="md5"):
+    for root, dirs, files in os.walk(directory_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            hash_value = singleMode(file_path, crypto)
+            print(f"File: {file_path}")
+            print(f"Hash ({crypto.upper()}): {hash_value}\n")
+
+# Recursive Mode
+Rpath = "tests"
+hashed = recursiveMode(Rpath, crypto="sha256")
+
+# Single Mode
+Spath = "tests/file.txt"
+hashed = singleMode(Spath, crypto="sha256")
+
 print(f"Hash value: {hashed}")
